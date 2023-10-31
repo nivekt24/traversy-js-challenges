15bf21cb6d9dd372c9c6bfe4b688db8310a1772c
Kevin Tran <nivekt24@Kevins-MacBook-Pro.local>
2023-10-31 15:46:04 -0700

    update solution

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,15 @@
-function reverseString() {}
+// Solution 1
+// const reverseString = (str) => str.split('').reverse('').join('');
+
+// Solution 2
+function reverseString(str) {
+  let reversed = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
 
 module.exports = reverseString;
